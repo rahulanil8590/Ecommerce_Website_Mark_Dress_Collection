e3f43607deff57accaadcba6bc009d79d2fbedb4
rahulanil8590 <rahulanilkumar152@gmail.com>
2023-08-15 06:59:37 +0530

    The File configured and  connected mongodb and model folder and controller folder are added.

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -19,8 +18,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// mongodb connecting
+const db =require('./config/connection');
+db.connect((err)=>{
+  if(err) console.log('database is connection error');
+  else console.log('Database is connected');
+})
+
+// Users Router
+const usersRouter = require('./routes/users');
+app.use('/', usersRouter);
+
+// Admin router
+var adminRouter = require('./routes/admin');
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
